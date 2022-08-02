baa09a139e67fd3000af53ca7f1184b6ec1d7a66
mohsin19295 <110176793+khanmohsin19295@users.noreply.github.com>
2022-08-02 19:54:44 +0530

    image upload completed

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let form = document.querySelector(".input-form");
 let displayDiv = document.querySelector(".display");
 
+
+let hname = document.getElementById("h2-hname")
 let fnameSpan = document.getElementById("fname-span");
 let lnameSpan = document.getElementById("lname-span");
 let citySpan = document.getElementById("city-span");
@@ -9,16 +11,17 @@ let hdegreeSpan = document.getElementById("hdegree-span");
 let cnameSpan = document.getElementById("cname-span");
 
 
+hname.innerHTML = "Mohammad Mohsin"
 fnameSpan.innerHTML = " " + "Mohammad";
 lnameSpan.innerHTML = " " + "Mohsin" + ".";
 citySpan.innerHTML = " " + "Gurgaon" + ",";
 stateSpan.innerHTML = " " + "Haryana" + ".";
-hdegreeSpan.innerHTML = " " + "12th";
-cnameSpan.innerHTML = " " + "xyz";
- 
-function handleForm(e) {
-  e.preventDefault();
+hdegreeSpan.innerHTML = " " + "B.tech";
+cnameSpan.innerHTML = " " + "Aligarh";
 
+function handleForm(e) {
+    e.preventDefault();
+   hname.innerHTML = document.getElementById("hname").value;
   fnameSpan.innerHTML = " " + form["fname"].value;
   lnameSpan.innerHTML = " " + form["lname"].value + ".";
   citySpan.innerHTML = " " + form["city"].value + ",";
@@ -26,6 +29,9 @@ function handleForm(e) {
   hdegreeSpan.innerHTML = " " + form["hdegree"].value;
   cnameSpan.innerHTML = " " + form["cname"].value;
 
+  if (hname.innerHTML.length == 0) {
+    hname.innerHTML = "Mohammad Mohsin";
+  }
   if (fnameSpan.innerHTML.length <= 1) {
     fnameSpan.innerHTML = " " + "Mohammad";
   }
@@ -39,17 +45,33 @@ function handleForm(e) {
     stateSpan.innerHTML = " " + "Haryana" + ".";
   }
   if (hdegreeSpan.innerHTML.length == 1) {
-    hdegreeSpan.innerHTML = " " + "12th";
+    hdegreeSpan.innerHTML = " " + "B.tech";
     }
-  if (cnameSpan.innerHTML.length == 2) {
-    cnameSpan.innerHTML = " " + "xyz";
+  if (cnameSpan.innerHTML.length == 1) {
+    cnameSpan.innerHTML = " " + "Aligarh";
     }
 }
 
 let options = document.getElementById("selectOption");
 let optionSpan = document.getElementById("option-span");
-optionSpan.innerHTML = " " + "school" + ".";
+optionSpan.innerHTML = " " + "university" + ".";
 
 options.addEventListener('change', () => {
-    optionSpan.innerHTML = " " + options.value + ".";
+    if (options.value == "select") {
+      optionSpan.style.display = "hidden";
+    } else {
+        optionSpan.innerHTML = " " + options.value + ".";
+    }
 })
+
+let readUrl = document.getElementById("readUrl")
+readUrl.addEventListener("change", function () {
+  if (this.files[0]) {
+    let picture = new FileReader();
+    picture.readAsDataURL(this.files[0]);
+      picture.addEventListener("load", function (e) {
+          let uploadedImg = document.getElementById("uploadedImg");
+          uploadedImg.setAttribute("src", e.target.result);
+    });
+  }
+});
