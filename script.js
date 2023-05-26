2015d2e15e3870b570e5312f03db57c894730898
mohsin19295 <mohsin19295@gmail.com>
2023-05-26 17:02:15 +0530

    data appending added

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let form = document.querySelector(".input-form");
 let displayDiv = document.querySelector(".display");
 
-
-let hname = document.getElementById("h2-hname")
+let hname = document.getElementById("h2-hname");
 let fnameSpan = document.getElementById("fname-span");
 let lnameSpan = document.getElementById("lname-span");
 let citySpan = document.getElementById("city-span");
@@ -10,8 +9,7 @@ let stateSpan = document.getElementById("state-span");
 let hdegreeSpan = document.getElementById("hdegree-span");
 let cnameSpan = document.getElementById("cname-span");
 
-
-hname.innerHTML = "Mohammad Mohsin"
+hname.innerHTML = "Mohammad Mohsin";
 fnameSpan.innerHTML = " " + "Mohammad";
 lnameSpan.innerHTML = " " + "Mohsin" + ".";
 citySpan.innerHTML = " " + "Gurgaon" + ",";
@@ -19,9 +17,11 @@ stateSpan.innerHTML = " " + "Haryana" + ".";
 hdegreeSpan.innerHTML = " " + "B.tech";
 cnameSpan.innerHTML = " " + "Aligarh";
 
+// Handeling form data
+
 function handleForm(e) {
-    e.preventDefault();
-   hname.innerHTML = document.getElementById("hname").value;
+  e.preventDefault();
+  hname.innerHTML = document.getElementById("hname").value;
   fnameSpan.innerHTML = " " + form["fname"].value;
   lnameSpan.innerHTML = " " + form["lname"].value + ".";
   citySpan.innerHTML = " " + form["city"].value + ",";
@@ -46,39 +46,87 @@ function handleForm(e) {
   }
   if (hdegreeSpan.innerHTML.length == 1) {
     hdegreeSpan.innerHTML = " " + "B.tech";
-    }
+  }
   if (cnameSpan.innerHTML.length == 1) {
     cnameSpan.innerHTML = " " + "Aligarh";
-    }
+  }
 }
 
 let options = document.getElementById("selectOption");
 let optionSpan = document.getElementById("option-span");
 optionSpan.innerHTML = " " + "university" + ".";
 
-options.addEventListener('change', () => {
-    if (options.value == "select") {
-      optionSpan.style.display = "hidden";
-    } else {
-        optionSpan.innerHTML = " " + options.value + ".";
-    }
-})
+options.addEventListener("change", () => {
+  if (options.value == "select") {
+    optionSpan.style.display = "hidden";
+  } else {
+    optionSpan.innerHTML = " " + options.value + ".";
+  }
+});
 
-let readUrl = document.getElementById("readUrl")
-readUrl.addEventListener("change", function () {
+// For image upload
+
+let readUrl = document.getElementById("readUrl");
+readUrl.addEventListener("change", imageViewer);
+let uploadedImg = document.getElementById("uploadedImg");
+
+function imageViewer() {
   if (this.files[0]) {
     let picture = new FileReader();
     picture.readAsDataURL(this.files[0]);
-      picture.addEventListener("load", function (e) {
-          let uploadedImg = document.getElementById("uploadedImg");
-          uploadedImg.setAttribute("src", e.target.result);
+    picture.addEventListener("load", function (e) {
+      uploadedImg.src = e.target.result;
     });
   }
-});
-  
-  function googleTranslateElementInit() {
+}
+
+// For Translate
+
+function googleTranslateElementInit() {
   new google.translate.TranslateElement(
     { pageLanguage: "en" },
     "google_translate_element"
   );
 }
+
+// For appending data
+
+let save = document.getElementById("save");
+let initialSpan = "Hi, my name is";
+let fromSpan = " I am from";
+let completedSpan = " I have completed my";
+let from = " from";
+
+let data = document.getElementById("data");
+
+save.addEventListener("click", () => {
+  let heading = hname.innerHTML;
+  let content =
+    initialSpan +
+    fnameSpan.innerHTML +
+    fromSpan +
+    lnameSpan.innerHTML +
+    fromSpan +
+    citySpan.innerHTML +
+    stateSpan.innerHTML +
+    completedSpan +
+    hdegreeSpan.innerHTML +
+    from +
+    cnameSpan.innerHTML +
+    optionSpan.innerHTML;
+  
+  let singleDiv = document.createElement("div");
+  singleDiv.className = "singleDiv"
+
+  let h4 = document.createElement("h4");
+  h4.innerHTML = heading;
+
+  let p = document.createElement("p");
+  p.innerHTML = content;
+
+  let img = document.createElement("img");
+  img.src = uploadedImg.src;
+
+  singleDiv.append(img, h4, p)
+  data.append(singleDiv);
+});
